f24cc4022df61cfc7bade2fbce0533273585f951
Adewale Osobu <aosobu@teamapt.com>
2020-07-19 15:19:23 +0100

    initial commit: testing ScrapperUtil dependency

---
+++ src/main/java/com/snowcoder/stockwatcher/StockwatcherApplication.java
@@ -0,0 +1,31 @@
+package com.snowcoder.stockwatcher;
+
+import com.snowcoder.webcrawlerlib.util.ScrapperUtil;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.snowcoder")
+public class StockwatcherApplication implements CommandLineRunner{
+	git remote add origin git@github.com:aosobu/stockwatcher.git
+
+	Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+	@Autowired
+	private ScrapperUtil scrapperUtil;
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockwatcherApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		Document document = scrapperUtil.connect("https://www.signetng.com/?pg=news");
+		scrapperUtil.getElementsFromDocument(document, "table.table.table-striped.table-hover tr");
+	}
+}
---
+++ src/test/java/com/snowcoder/stockwatcher/StockwatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowcoder.stockwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockwatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
