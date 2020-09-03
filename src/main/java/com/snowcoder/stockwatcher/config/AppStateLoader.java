89d41e453094ccfefdb48f12113f69e9b045c3db
Adewale Osobu <aosobu@teamapt.com>
2020-09-03 06:59:41 +0100

    starts implementation for InitializedAppStateService

--- src/main/java/com/snowcoder/stockwatcher/config/AppStateLoader.java
+++ src/main/java/com/snowcoder/stockwatcher/config/AppStateLoader.java
@@ -1,6 +1,7 @@
 package com.snowcoder.stockwatcher.config;
 
 import com.snowcoder.stockwatcher.model.AppState;
+import com.snowcoder.stockwatcher.service.AppContextState;
 import com.snowcoder.stockwatcher.service.AppStateServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 @DependsOn("appStateSetUpMap")
@@ -17,20 +20,27 @@ public class AppStateLoader{
 
     private AppStateServiceImpl appStateServiceImpl;
     private final Logger LOGGER = LoggerFactory.getLogger(AppStateLoader.class);
+    private Map<com.snowcoder.stockwatcher.enums.AppState, AppContextState> appStateSetUpMap;
+
 
     @Bean
     public void checkAppState(){
         LOGGER.info("============ Setting up AppStateLoader =================");
-        LOGGER.info("============ CHECKING APP STATE =================");
         List<AppState> appState = appStateServiceImpl.retrieveAppState();
-        if(appState.isEmpty()) {
-            // initiate app set up
-            LOGGER.info("============ INITIALIZING APPs =================");
+        if(appState.isEmpty() || !appState.get(0).getDatabaseIsFilled()) {
+            appStateSetUpMap.get(com.snowcoder.stockwatcher.enums.AppState.UNINITIALIZED).setUpState();
+            return;
         }
+        appStateSetUpMap.get(com.snowcoder.stockwatcher.enums.AppState.INITIALIZED).setUpState();
     }
 
     @Autowired
     public void setAppStateServiceImpl(AppStateServiceImpl appStateServiceImpl) {
         this.appStateServiceImpl = appStateServiceImpl;
+    }
+
+    @Resource
+    public void setAppStateSetUpMap(Map<com.snowcoder.stockwatcher.enums.AppState, AppContextState> appStateSetUpMap) {
+        this.appStateSetUpMap = appStateSetUpMap;
     }
 }
